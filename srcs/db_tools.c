199ffe3a5c0a115d34773d03eeeb860992ca7057
acuD1 <arsciand@student.42.fr>
2019-02-28 18:13:34 +0100

    stat inc

--- srcs/db_tools.c
+++ srcs/db_tools.c
@@ -6,20 +6,29 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 14:19:04 by arsciand          #+#    #+#             */
-/*   Updated: 2019/02/28 17:34:55 by arsciand         ###   ########.fr       */
+/*   Updated: 2019/02/28 18:10:11 by arsciand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
+void	get_permission(struct stat db_stat)
+{
+	(void)db_stat;
+	char *tmp;
+
+	tmp = ft_strnew(10);
+	printf("TEST = |%s|\n", tmp);
+}
+
 t_ls	*fetch_db(t_ls *db, char *av)
 {
-	struct stat	d_stat;
+	struct stat	db_stat;
 
-	lstat(av, &d_stat);
+	lstat(av, &db_stat);
 	db->var = ft_strdup(av);
-	db->type = get_type(d_stat);
-	printf("DEBUG | db->var = |%s|\n", db->var);
-	printf("DEBUG | db->type = |%c|\n", db->type);
+	db->type = get_type(db_stat);
+	printf("Mode : %lo (octal)\n", (unsigned long) db_stat.st_mode);
+	get_permission(db_stat);
 	return (db);
 }
--- srcs/get_type.c
+++ srcs/get_type.c
@@ -6,16 +6,28 @@
 /*   By: arsciand <arsciand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 14:22:46 by arsciand          #+#    #+#             */
-/*   Updated: 2019/02/28 14:28:56 by arsciand         ###   ########.fr       */
+/*   Updated: 2019/02/28 17:55:13 by arsciand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-char	get_type(struct stat d_stat)
+char	get_type(struct stat db_stat)
 {
-	if (S_ISDIR(d_stat.st_mode) == 1)
+	if (S_ISREG(db_stat.st_mode))
+		return ('-');
+	else if (S_ISDIR(db_stat.st_mode))
 		return ('.');
+	else if (S_ISCHR(db_stat.st_mode))
+		return ('c');
+	else if (S_ISBLK(db_stat.st_mode))
+		return ('b');
+	else if (S_ISFIFO(db_stat.st_mode))
+		return ('p');
+	else if (S_ISLNK(db_stat.st_mode))
+		return ('l');
+	else if (S_ISSOCK(db_stat.st_mode))
+		return ('s');
 	else
 		return ('?');
 }
